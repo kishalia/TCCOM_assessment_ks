65ef5d45a13c29ac998c17766e550c9eee220fbc
kishalia <158999987+kishalia@users.noreply.github.com>
2024-07-02 08:55:23 +1200

    Create C06_pandaV1_trialling_VER5.py
    Refinement of pandas (test plan one)

---
+++ C06_pandaV1_trialling_VER5.py
@@ -0,0 +1,13 @@
+# Sample data 
+distance = [2]
+gradient = [1.5]
+midpoint = [(1, 2)]
+equation = ['y = 2x + 1']
+
+# Print the output
+print(" -COORDINATE GEOMETRY CALCULATOR-")
+print(f" Coordinates given:        _")
+print(f" Distance between X and Y: {','.join(map(str, distance))}")
+print(f" Gradient of the line:    {',  '.join(map(str, gradient))}")
+print(f" Midpoint of the line:    {', '.join(map(str, midpoint))}")
+print(f" Equation of the line:    {',  '.join(map(str, equation))}")
